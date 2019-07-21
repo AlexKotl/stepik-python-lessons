0ca5d7aad0b0acf6daed11022f25c08b0c5d94ce
Alex <slicer256@gmail.com>
2019-07-21 23:39:03 +0300

    3 chapter solution

---
+++ 3/solution.py
@@ -0,0 +1,15 @@
+
+
+class FileReader:
+    def __init__(self, filename):
+        self.filename = filename
+        
+    def read(self):
+        try:
+            with open(self.filename, 'r') as f:
+                return f.read()
+        except IOError:
+            return ""
+
+reader = FileReader("example.txt")
+print(reader.read())
