24ddda292f04b21681803c00a7c765a9d358e499
Alex <slicer256@gmail.com>
2019-07-23 19:06:04 +0300

    matrix class update

--- 3/matrix.py
+++ 3/matrix.py
@@ -2,16 +2,54 @@ class Matrix:
     MAX_SIZE = 1000  
 
     def __init__(self, max_size=None):
-        pass
+        self.matrix = [[None]]
+        self.MAX_SIZE = max_size or self.MAX_SIZE
+        
+    def __extend_matrix(self):
+        list = []
+        for row in self.matrix:
+            for el in row:
+                list.append(el)
+        
+        self.matrix = self.__create_matrix(len(self.matrix) + 1)
+        
+        for el in list:
+            self.append(el)
+        return self.matrix
+        
+    def __create_matrix(self, size):    
+        matrix = [[None for x in range(size)] for y in range(size)] 
+        return matrix
 
     def append(self, element=None):
-        pass
+        if element == None:
+            return self.matrix
+            
+        size = len(self.matrix)
+        for x in range(size):
+            for y in range(size):
+                if self.matrix[x][y] == None:
+                    if x == size - 1:
+                        self.__extend_matrix()
+                        self.append(element)
+                        return self.matrix
+                    self.matrix[x][y] = element
+                    return self.matrix
+        return matrix
 
     def pop(self):
         pass
 
     def __str__(self):
-        pass
+        result = ''
+        for x in range(len(self.matrix)):
+            if x > 0:
+                result += '\n'
+            for y in range(len(self.matrix)):
+                if (y > 0):
+                    result += ' '
+                result += str(self.matrix[x][y])
+        return result
 
     @classmethod
     def from_iter(cls, iter_obj, max_size=None):
---
+++ 3/test_matrix.py
@@ -0,0 +1,10 @@
+import matrix
+
+matrix = matrix.Matrix()
+for i in range(1, 4):
+    matrix.append(i)
+    print(matrix)
+    print('*' * 10)
+    
+#matrix.pop()
+#print(matrix)
