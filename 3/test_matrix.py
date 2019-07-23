db1c88c08240a3b8905f666d0a3e1fa6b08770fb
Alex <slicer256@gmail.com>
2019-07-23 22:06:59 +0300

    matrix time test

--- 3/test_matrix.py
+++ 3/test_matrix.py
@@ -1,23 +1,32 @@
 from matrix import Matrix
+from time import time
 
-# matrix = matrix.Matrix()
-# for i in range(1, 4):
-#     matrix.append(i)
-#     print(matrix)
-#     print('*' * 10)
-# 
-# matrix.pop()
-# print(matrix)
-# 
-# matrix.append(None)
-# print(matrix)
+start = time()
+matrix = Matrix()
+for k in range(10):
+    matrix.append(k)
+end = time()
+print(end - start)
+quit()
 
-# matrix = matrix.Matrix(max_size=2)
-# for i in range(1, 5):
-#     matrix.append(i)
-#     print(matrix)
-#     print('#' * 10)
-# matrix.append(5)
+matrix = Matrix()
+for i in range(1, 4):
+    matrix.append(i)
+    print(matrix)
+    print('*' * 10)
+
+matrix.pop()
+print(matrix)
+
+matrix.append(None)
+print(matrix)
+
+matrix = Matrix(max_size=2)
+for i in range(1, 5):
+    matrix.append(i)
+    print(matrix)
+    print('#' * 10)
+matrix.append(5)
 
 matrix = Matrix.from_iter([1,2,3])
 print(matrix)
