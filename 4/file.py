49302b7cdbe9a3dc856e96f1de16a0f55a37f34c
Alex <slicer256@gmail.com>
2019-07-28 13:12:12 +0300

    file task completed

--- 4/file.py
+++ 4/file.py
@@ -2,5 +2,32 @@ import tempfile
 import os
 
 class File:
-    def __init__(self):
-        pass
+    def __init__(self, filename):
+        self.filename = filename
+
+    def write(self, content):
+        with open(self.filename, 'w') as f:
+            f.write(content)
+    
+    def read(self):
+        with open(self.filename, 'r') as f:
+            return f.read()
+        
+    def __str__(self):
+        return self.filename
+        
+    def __add__(self, file2):
+        new_filename = os.path.basename(self.filename) + '.' + os.path.basename(file2.filename)
+        new_file = File(os.path.join(tempfile.gettempdir(), new_filename)) 
+        new_file.write(self.read() + file2.read())
+        return new_file
+        
+    def __iter__(self):
+        self.f = open(self.filename, "r")
+        return self
+        
+    def __next__(self):
+        line = self.f.readline()
+        if not line:
+            raise StopIteration
+        return line
--- 4/test_file.py
+++ 4/test_file.py
@@ -1,8 +1,8 @@
 from file import File
 
-file1 = File('test1.txt')
-file2 = File('test2.txt')
-file1.write('String for test1')
+file1 = File('/domains/test1.txt')
+file2 = File('/domains/test2.txt')
+file1.write('String for test1\nsecond\nstr')
 file2.write('String for test2')
 
 new_file = file1 + file2
@@ -11,10 +11,10 @@ print(file1)
 print(file2)
 print(new_file)
 
-file1.read()
-file2.read()
+print(file1.read())
+print(file2.read())
 
-new_file.read()
+print(new_file.read())
 
-import os.path
-os.path.exists(r'C:\Users\Media\AppData\Local\Temp\new_file_from_test1.txt_and_test2.txt')
+for line in new_file:
+    print(f"line:{line}")
