64bc90a6d558b64b559da8862859ce85809a5ec2
Alex <slicer256@gmail.com>
2019-07-29 15:04:55 +0300

    calendar task template

---
+++ 4/calendar.py
@@ -0,0 +1,8 @@
+class CalendarBookmark:
+    """класс дескриптор - закладка для ежедневника"""
+
+
+class CalendarBook(Book):
+    """класс книги - ежедневник с закладкой"""
+    def __init__(self, title, content=None):
+        pass
