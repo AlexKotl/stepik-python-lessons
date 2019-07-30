f60be64b579b85021853d3d5f84c2c996201920a
Alex <slicer256@gmail.com>
2019-07-30 23:43:07 +0300

    client base methods

--- 5/client.py
+++ 5/client.py
@@ -1,5 +1,12 @@
 class Client:
-    pass
+    def __init__(self, host, port, timeout=None):
+        pass
+    
+    def get(self, metric):
+        pass
+        
+    def put(self, metric, value, timestamp=None):
+        pass
     
 class ClientError(Exception):
     pass
