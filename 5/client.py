fd120730cca3d7eb45dedbc6d8cbdc5968ae4aed
Alex <slicer256@gmail.com>
2019-07-31 23:20:30 +0300

    client passed local tests

--- 5/client.py
+++ 5/client.py
@@ -1,12 +1,37 @@
+import socket
+
 class Client:
     def __init__(self, host, port, timeout=None):
-        pass
+        self.sock = socket.create_connection((host, port), timeout)
     
-    def get(self, metric):
-        pass
+    def get(self, key):
+        result = {}
+        try:
+            self.sock.send(f"get {key}\n".encode())
+            data = self.sock.recv(1024).decode("utf8").split("\n")
+            if data[0] == 'error':
+                raise ClientError
+            for row in data[1:]:
+                if row == '':
+                    continue
+                row = row.split(' ')
+                name = row[0]
+                value = float(row[1])
+                timestamp = int(row[2])
+                
+                if name not in result:
+                    result[name] = []
+                result[name].append((timestamp, value))
+        except socket.error as err:
+            print("Get data error:", err)
+            raise ClientError
         
-    def put(self, metric, value, timestamp=None):
-        pass
+        return result
+        
+    def put(self, key, value, timestamp=None):
+        timestamp = timestamp or str(int(time.time()))
+        string = f"put {key} {value} {timestamp}\n"
+        self.sock.sendall(string.encode())
     
 class ClientError(Exception):
     pass
