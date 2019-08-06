15a66961628d0028b573fbea470658c82d672adb
Alex <slicer256@gmail.com>
2019-08-06 22:13:38 +0300

    regexp task completed

--- w1/regexp.py
+++ w1/regexp.py
@@ -1,7 +1,11 @@
 def calculate(data, findall):
-    matches = findall(r"([abc])([\-\+])?=([abc])?(([\-\+])?\d*)?")  # Если придумать хорошую регулярку, будет просто
+    matches = findall(r"([abc])([\-\+])?=([abc])?([\-\+]?\d*)?")  # Если придумать хорошую регулярку, будет просто
+    print(matches)
     for v1, s, v2, n in matches:  # Если кортеж такой структуры: var1, [sign]=, [var2], [[+-]number]
         # Если бы могло быть только =, вообще одной строкой все считалось бы, вот так:
-        data[v1] = data.get(v2, 0) + int(n or 0)
+        if s == '':
+            data[v1] = data.get(v2, 0) + int(n or 0)
+        else:
+            data[v1] += (-1 if s == '-' else 1) * (data.get(v2, 0) + int(n or 0))
 
     return data
