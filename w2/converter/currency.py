e461f34ebb315d927665b11966d03fe5311c7d53
Alex <slicer256@gmail.com>
2019-08-19 18:35:07 +0300

    working calculator

--- w2/converter/currency.py
+++ w2/converter/currency.py
@@ -5,6 +5,17 @@ from decimal import Decimal
 def convert(amount, cur_from, cur_to, date, requests):
     response = requests.get(f'http://www.cbr.ru/scripts/XML_daily.asp?date_req={date}')
     soup = BeautifulSoup(response.content, 'xml')
-
-    result = Decimal('3754.8057')
-    return result  
+    data = {}
+    for cur in (cur_from, cur_to):
+        if cur == 'RUR':
+            data.update({ cur: {
+                'value': Decimal(1),
+                'nominal': 1
+            } })
+        else:
+            data.update({ cur: {
+                'value': Decimal(soup.find('CharCode', text=cur).find_next_sibling('Value').string.replace(',', '.')),
+                'nominal': int(soup.find('CharCode', text=cur).find_next_sibling('Nominal').string)
+            } })
+    result = amount * data[cur_from]['nominal'] * data[cur_from]['value'] / data[cur_to]['value'] * data[cur_to]['nominal']
+    return Decimal(result).quantize(Decimal('1.0000'))
