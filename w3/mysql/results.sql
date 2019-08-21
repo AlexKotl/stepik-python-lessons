af67547ced3abb21069be1f5c5267104d6a7226c
Alex <slicer256@gmail.com>
2019-08-21 14:13:58 +0300

    all queries

--- w3/mysql/results.sql
+++ w3/mysql/results.sql
@@ -23,13 +23,19 @@ natural join product
 group by product_id
 
 -- 7. Получить названия всех продуктов, которые продавались только с единственного склада
-select ...
+select name from product
+NATURAL JOIN sale
+group by name having count(distinct store_id) = 1
 
 -- 8. Получить названия всех складов, с которых продавался только один продукт
-select ...
+select name as products from sale
+natural join store
+group by name having count(distinct product_id) = 1
 
 -- 9. Выберите все ряды (все поля) из продаж, в которых сумма продажи (total) максимальна (равна максимальной из всех встречающихся)
-select ...
+select * from sale where total = (select max(total) from sale)
 
 -- 10. Выведите дату самых максимальных продаж, если таких дат несколько, то самую раннюю из них
-select ...
+select date from sale 
+group by date
+order by sum(quantity) desc, date limit 1
