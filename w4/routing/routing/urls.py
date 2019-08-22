33166859f37d4b246d6eee9df748cb7bf068bdd8
Alex <slicer256@gmail.com>
2019-08-22 12:11:07 +0300

    simple route

--- w4/routing/routing/urls.py
+++ w4/routing/routing/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
+from routing.views import simple_route
 
 urlpatterns = [
-
+    url(r'^simple_route/', simple_route)
 ]
---
+++ w4/routing/routing/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def simple_route(request):
+    code = 200 if request.method == 'GET' else 405
+    return HttpResponse('', status=code)
