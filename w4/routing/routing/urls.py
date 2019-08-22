8e0014d7b9659b0d372509eb5db3bbe61e2645bc
Alex <slicer256@gmail.com>
2019-08-22 14:34:32 +0300

    slug route

--- w4/routing/routing/urls.py
+++ w4/routing/routing/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from routing.views import simple_route
+from routing.views import *
 
 urlpatterns = [
-    url(r'^simple_route/', simple_route)
+    url(r'^simple_route/', simple_route),
+    url(r'^slug_route/([0-9a-z\-_]{1,16})', slug_route)
 ]
--- w4/routing/routing/views.py
+++ w4/routing/routing/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def simple_route(request):
     code = 200 if request.method == 'GET' else 405
     return HttpResponse('', status=code)
+    
+def slug_route(request, slug):
+    return HttpResponse(slug)
+    
