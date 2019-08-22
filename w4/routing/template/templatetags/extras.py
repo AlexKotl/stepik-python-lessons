54283f05598f1752c54712a3d5d6af9a07dd6cdb
Alex <slicer256@gmail.com>
2019-08-22 16:08:39 +0300

    tags and filter

--- w4/routing/template/templatetags/extras.py
+++ w4/routing/template/templatetags/extras.py
@@ -1,1 +1,16 @@
 from django import template
+
+register = template.Library()
+
+@register.filter
+def inc(value, param):
+    return int(value) + int(param)
+    
+@register.simple_tag
+def division(a, b, **kwargs):
+    a = int(a)
+    b = int(b)
+    if 'to_int' in kwargs and kwargs['to_int']:
+        return a // b
+    else:
+        return a / b
