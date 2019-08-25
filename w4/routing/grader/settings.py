44999858c97a5521665a29a05fa5d525255168e4
Alex <slicer256@gmail.com>
2019-08-25 19:27:01 +0300

    added alt database setting

--- w4/routing/grader/settings.py
+++ w4/routing/grader/settings.py
@@ -82,7 +82,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
     }
+    
 }
 
 
